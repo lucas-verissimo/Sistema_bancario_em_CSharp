485964e93753ff244824c123101afcaf87a2222f
Lucas Veríssimo <verissimo.lucas1994@gmail.com>
2021-07-12 11:17:48 -0300

    Update

---
+++ Lista.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class Lista<T>
+    {
+        private T[] _itens;
+        private int _proximaPosicao;
+
+        public int Tamanho
+        {
+            get
+            {
+                return _proximaPosicao;
+            }
+        }
+
+        public Lista(int capacidadeInicial = 5)
+        {
+            _itens = new T[capacidadeInicial];
+            _proximaPosicao = 0;
+        }
+
+        public void MeuMetodo(string texto = "texti padrao", int numero = 5)
+        {
+
+        }
+
+        public void Adicionar(T item)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+
+
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+
+        public void AdicionarVarios(params T[] itens)
+        {
+            foreach (T item in itens)
+            {
+                Adicionar(item);
+            }
+        }
+
+        public void Remover(T item)
+        {
+            int indiceItem = -1;
+
+            for (int i = 0; i < _proximaPosicao; i++)
+            {
+                T itemAtual = _itens[i];
+
+                if (itemAtual.Equals(item))
+                {
+                    indiceItem = i;
+                    break;
+                }
+            }
+
+
+            for (int i = indiceItem; i < _proximaPosicao - 1; i++)
+            {
+                _itens[i] = _itens[i + 1];
+            }
+
+            _proximaPosicao--;
+        }
+
+        public T GetItemNoIndice(int indice)
+        {
+            if (indice < 0 || indice >= _proximaPosicao)
+            {
+                throw new ArgumentOutOfRangeException(nameof(indice));
+            }
+
+            return _itens[indice];
+        }
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if (_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            int novoTamanho = _itens.Length * 2;
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+
+            T[] novoArray = new T[novoTamanho];
+
+            for (int indice = 0; indice < _itens.Length; indice++)
+            {
+                novoArray[indice] = _itens[indice];
+            }
+
+            _itens = novoArray;
+        }
+
+        public T this[int indice]
+        {
+            get
+            {
+                return GetItemNoIndice(indice);
+            }
+        }
+    }
+}
---
+++ ListaDeContaCorrente.cs
@@ -0,0 +1,121 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+
+
+    public class ListaDeContaCorrente
+    {
+        private ContaCorrente[] _itens;
+        private int _proximaPosicao;
+        
+        public int Tamanho
+        {
+            get
+            {
+                return _proximaPosicao;
+            }
+        }
+
+        public ListaDeContaCorrente(int capacidadeInicial = 5)
+        {
+            _itens = new ContaCorrente[capacidadeInicial];
+            _proximaPosicao = 0;
+        }
+
+        public void MeuMetodo(string texto = "texti padrao", int numero = 5)
+        {
+
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+
+
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+        
+        public void AdicionarVarios(params ContaCorrente[] itens)
+        {
+            foreach(ContaCorrente conta in itens)
+            {
+                Adicionar(conta);
+            }
+        }
+
+        public void Remover(ContaCorrente item)
+        {
+            int indiceItem = -1;
+
+            for(int i = 0; i < _proximaPosicao; i++)
+            {
+                ContaCorrente itemAtual = _itens[i];
+
+                if (itemAtual.Equals(item))
+                {
+                    indiceItem = i;
+                    break;
+                }
+            }
+
+
+            for (int i = indiceItem; i < _proximaPosicao-1; i++)
+            {
+                _itens[i] = _itens[i + 1];
+            }
+
+            _proximaPosicao--;
+            _itens[_proximaPosicao] = null;
+        }
+
+        public ContaCorrente GetItemNoIndice(int indice)
+        {
+            if(indice < 0 || indice >= _proximaPosicao)
+            {
+                throw new ArgumentOutOfRangeException(nameof(indice));
+            }
+
+            return _itens[indice];
+        }
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if(_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            int novoTamanho = _itens.Length * 2;
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+
+            ContaCorrente[] novoArray = new ContaCorrente[novoTamanho];
+
+            for(int indice = 0; indice < _itens.Length; indice++)
+            {
+                novoArray[indice] = _itens[indice];
+            }
+
+            _itens = novoArray;
+        }
+        
+        public ContaCorrente this[int indice]
+        {
+            get
+            {
+                return GetItemNoIndice(indice);
+            }
+        }
+
+    }
+}
